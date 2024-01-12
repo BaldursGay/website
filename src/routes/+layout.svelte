4f4ae3239331f290f4049c778b9b8a6c0a0c97cf
Jade Nash <jade@lilydev.com>
2024-01-12 15:09:09 -0600

    Add dark/light transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 
 <div class={$theme}>
 	<div
-		class="flex flex-col justify-center place-items-center w-screen h-screen px-8 py-3 md:px-56 xl:px-72 text-zinc-900 dark:text-gray-100 bg-gray-100 dark:bg-zinc-900"
+		class="flex flex-col justify-center place-items-center w-screen h-screen px-8 py-3 md:px-56 xl:px-72 transition-colors duration-500 text-zinc-900 dark:text-gray-100 bg-gray-100 dark:bg-zinc-900"
 	>
 		<div class="flex grow">
 			<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,10 @@
 
 <div class="flex place-items-center gap-2">
 	<a
-		class="p-2 transition-all hover:bg-gray-200 hover:dark:bg-zinc-800 rounded-full"
+		class="p-2 transition-all scale-100 active:scale-95 hover:bg-gray-200 hover:dark:bg-zinc-800 rounded-full"
 		href="https://github.com/BaldursGay/"
 	>
-		<Github
-			class="w-10 transition-all scale-100 active:scale-95 text-zinc-900 dark:text-gray-100"
-		/>
+		<Github class="w-10 transition-colors duration-500 text-zinc-900 dark:text-gray-100" />
 	</a>
 	<h1 class="text-4xl font-semibold">Coming soon...</h1>
 </div>
